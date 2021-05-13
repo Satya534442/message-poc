6d8853b9c26153e6d0941bbddb5595b754475809
Satya534442 <34699378+Satya534442@users.noreply.github.com>
2021-05-13 10:12:10 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ import CurrentSelectedBar from './components/CurrentSelectedBar';
 import * as mockData from './__mock__/mock.json';
 import { formatMessageDataForNav } from './utils/utils';
 import LoaderSkelton from './components/Skeleton';
+import styled from 'styled-components';
+import bgImage from './assets/weathered-blue-page.jpg';
+import loader from './assets/Flip Flop.gif';
+
+const Wrapper = styled.div`
+  background-image: url(${bgImage});
+`;
 
 function App() {
   const [fetching, setFetching] = useState({
     isLoading: true,
     isSuccess: false,
+    isMessageFetching: true,
     data: [],
   });
 
@@ -24,15 +32,21 @@ function App() {
       setFetching({
         isLoading: false,
         isSuccess: true,
-        data: mockData.default.messages,
+        data: mockData.default.messages.map(item => ({ ...item, id: Math.random().toString(16).slice(2)})),
         isMessageFetching: true,
+        isDeleting: false,
       })
+      setSelectedPhoneNumber(Object.keys(formatMessageDataForNav(mockData.default.messages))[0]);
     }, 1000);
     return () => clearTimeout(timer);
   }, []);
 
   useEffect(() => {
     if(selectedPhoneNumber) {
+      setFetching({
+        ...fetching,
+        isMessageFetching: true,
+      })
       const timer = setTimeout(() => {
         setFetching({
           ...fetching,
@@ -62,100 +76,84 @@ function App() {
     setAddNewNumber(false);
     setSelectedPhoneNumber(number);
   }
-  
+  // change it to redux pattern
+  const deleteNumberOrMessage = (number = '', messageId = '', numberMessageArr = {}) => {
+    const savedStoreData = fetching.data;
+
+    if(number) {
+      setFetching({
+        ...fetching,
+        isDeleting: true
+      });
+      const filterData = savedStoreData.filter(item => item.number.toString() !== number.toString());
+      setFetching({
+        ...fetching,
+        data: filterData,
+        isDeleting: false,
+      });
+      setSelectedPhoneNumber(Object.keys(formatMessageDataForNav(filterData))[0]);
+    }
+
+    if(messageId) {
+      setFetching({
+        ...fetching,
+        isDeleting: true
+      });
+      const filterData = savedStoreData.filter(item => item.id !== messageId);
+      setFetching({
+        ...fetching,
+        data: filterData,
+        isDeleting: false,
+      });
+      const tempMessages = formatMessageDataForNav(filterData)[selectedPhoneNumber];
+      if(tempMessages && tempMessages.length >0) {
+        setMessages(formatMessageDataForNav(filterData)[selectedPhoneNumber])
+      } else {
+        setSelectedPhoneNumber(Object.keys(formatMessageDataForNav(filterData))[0]);
+      }
+    }
+    if(Object.keys(numberMessageArr).length) {
+      const selectedNumbers = Object.keys(numberMessageArr).filter(item => numberMessageArr[item]);
+      const filteredArr = savedStoreData.filter(item => !selectedNumbers.includes(item.number))
+      setFetching({
+        ...fetching,
+        data: filteredArr,
+        isDeleting: false,
+      });
+    }
+  }
 
   return (
     <div className="wrapper">
       <div className="sidenav-bar">
-        {fetching.data.length > 0 && 
+        {fetching.data && fetching.data.length > 0 && 
           <SideNavBar 
             data={formatMessageDataForNav(fetching.data)} 
             doSelect={setSelectedPhoneNumber} 
             setAddNewNumber={setAddNewNumber}
             isAdd={addNewNumber}
+            selected={selectedPhoneNumber}
+            deleteNumberOrMessage={deleteNumberOrMessage}
           />
         }
         {fetching.isLoading && !fetching.isSuccess && (
-          <div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
+          <div class="loader">
+            <img src={loader} alt="Loading" />
           </div>
         )}
       </div>
       <div className="messageSection">
-        {fetching.data.length > 0 && selectedPhoneNumber && messages.length > 0 &&
+        {fetching.data && fetching.data.length > 0 && selectedPhoneNumber && messages.length > 0 &&
           <CurrentSelectedBar 
             data={messages} 
             number={selectedPhoneNumber} 
             addNewNumberOrMessage={addNewNumberOrMessage}
             isAdd={addNewNumber}
+            deleteNumberOrMessage={deleteNumberOrMessage}
           />}
-        {!fetching.isLoading && fetching.isSuccess && fetching.isMessageFetching && (
-          <div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
-            <div>
-              <LoaderSkelton width="100%" height="75px" />
-            </div>
+        {(fetching.isLoading || fetching.isSuccess) && (fetching.isMessageFetching || fetching.isDeleting) && (
+          <div class="loader">
+            <img src={loader} alt="Loading" />
           </div>
         )}
       </div>
